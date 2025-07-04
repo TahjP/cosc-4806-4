f2e7f707702e4292bec811a1c17df13e7ebe8663
TahjP <tplummer@algomau.ca>
2025-07-04 04:08:41 +0000

    added the form to the create page

--- app/views/reminders/create.php
+++ app/views/reminders/create.php
@@ -7,5 +7,14 @@
             </div>
         </div>
     </div>
+    <form action="/reminders/store" method="post">
+        <div class="form-group">
+            <label for="reminder">Reminder</label>
+            <input required type="text" class="form-control" name="reminder">
+        </div>
+        <br>
+        <button type="submit" class="btn btn-primary">Save</button>
+        <a href="/reminders" class="btn btn-secondary mt-2">Back</a>
+    </form>
 </div>
 <?php require_once 'app/views/templates/footer.php' ?>
