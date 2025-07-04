46f2c1bcaa98f3086579501ab82f70f59bfbdb64
TahjP <tplummer@algomau.ca>
2025-07-04 01:40:15 +0000

    gave the reminders controller functionality

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -1,8 +1,12 @@
 <?php
 
 class Reminders extends Controller {
-    public function index()
-  {
+    public function index() {
+      $reminder = $this->model('Reminder');
+      $list_of_reminders = $reminder->get_all_reminders();
+      print_r ($list_of_reminders);
+      die;
       $this->view('reminders/index');
+
   }
 }
