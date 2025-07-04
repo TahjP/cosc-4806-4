f60e453f47352e9d304f1c2baae64f1400dffa18
TahjP <tplummer@algomau.ca>
2025-07-04 03:09:57 +0000

    made create link and function for the reminders page to create a function

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -2,11 +2,13 @@
 
 class Reminders extends Controller {
     public function index() {
-      $reminder = $this->model('Reminder');
-      $list_of_reminders = $reminder->get_all_reminders();
-      print_r ($list_of_reminders);
-      die;
-      $this->view('reminders/index');
+      $R = $this->model('Reminder');
+      $list_of_reminders = $R->get_all_reminders();
+      $this->view('reminders/index', ['reminders' => $list_of_reminders]);
 
   }
+    public function create() {
+        $R = $this->model('Reminder');
+        $this->view('reminders/create');
+    }
 }
--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -4,9 +4,10 @@
         <div class="row">
             <div class="col-lg-12">
                 <h1>Reminders</h1>
+                <p> <a href="/reminders/create">Click here to create a new reminder</a></p>"
             </div>
         </div>
     </div>
 </div>
-
+<table></table>
 <?php require_once 'app/views/templates/footer.php' ?>
