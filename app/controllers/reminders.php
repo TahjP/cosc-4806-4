03775715086a3982bd7407d0b9f1b02a31e14fe0
TahjP <tplummer@algomau.ca>
2025-07-04 03:49:30 +0000

    prepared to store, edit, update and delete reminders

--- app/controllers/reminders.php
+++ app/controllers/reminders.php
@@ -2,13 +2,24 @@
 
 class Reminders extends Controller {
     public function index() {
-      $R = $this->model('Reminder');
-      $list_of_reminders = $R->get_all_reminders();
-      $this->view('reminders/index', ['reminders' => $list_of_reminders]);
+              $R = $this->model('Reminder');
+              $list_of_reminders = $R->get_all_reminders();
+              $this->view('reminders/index', ['reminders' => $list_of_reminders]);
 
-  }
+          }
     public function create() {
-        $R = $this->model('Reminder');
         $this->view('reminders/create');
+    }
+    public function store() {
+
+    }
+    public function edit() {
+        
+    }
+    public function update() {
+        
+    }
+    public function delete(){
+        
     }
 }
